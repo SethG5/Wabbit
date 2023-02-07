2d2f26f3d1f1bc03d127b1f44ca8f147c9031978
Seth <124543219+SethG5@users.noreply.github.com>
2023-02-07 11:07:47 -0800

    more features
    added a help command and a hash command.

--- main/main.py
+++ main/main.py
@@ -24,6 +24,7 @@ import webbrowser
 from platform import system
 from time import sleep
 import hashlib
+from modules import AntHashLib
 import random
 import socket
 from builtins import input
@@ -45,7 +46,7 @@ file = 'wabbit'
 dir = '~/' + file
 
 class Constant:
-  Prompt = f'''\033[31m┌─[{user}\033[1;33m@\033[0;36m\033[31m]─[{dir}]\n└──╼ #'''
+  Prompt = f'''\033[31m┌─[{user}\033[1;33m@\033[0;36m\033[31m]─[{dir}]\n└──╼ $\033[1;37m '''
   user = socket.gethostname()
   logo = '''\033[1;37m
    ***       
@@ -89,12 +90,36 @@ def main():
           hashid.hashid(hash)
       except IndexError:
         print('Usage: hashid [hash]')
+    if mainCommand == 'hash':
+      try:
+        string = command[1]
+        hashType = command[2]
+        hashType = hashType.lower()
+        string = AntHashLib.String(string)
+        exec(f'print(string.{hashType}())')
+      except AttributeError:
+        print('Hash not found.')
+      except SyntaxError:
+        print('Error, put the hashtype in plain text')
+      except IndexError:
+        print('Usage: hash [string] [hashType]')
 
 
 #######################
 # RUN
 #######################
 
+    if mainCommand == 'wizard':
+      print('not finished yet')
+    if mainCommand == 'help':
+      print('''
+      hashid [hash]            | identifies possible hashes
+      hash [string] [hashType] | hashes a string
+      help                     | take a guess
+      wizard                   | Opens a more simple version of Wabbit
+      ''')
+    else:
+      print('Command not found. Try running help.)
 if __name__ == '__main__':      
   main()
   
