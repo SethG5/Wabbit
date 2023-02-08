dc4f05f1a9a91a7207dab3b51babd81ae6344882
Seth <124543219+SethG5@users.noreply.github.com>
2023-02-08 09:45:45 -0800

    Update randomBrute.py

--- main/modules/randomBrute.py
+++ main/modules/randomBrute.py
@@ -1,1 +1,20 @@
+import random
+import AntHashLib
 def Brute(hash, hashType, Fixed, len):
+    cracked == False
+    if Fixed == True:
+        strLen = len
+    elif Fixed == False:
+        strLen = random.Randint(1, len)
+    for i in range(0, strLen):
+        text = random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+    text = AntHashLib.String(text)
+    while cracked == False:
+        try:
+            if hash == text.hash(hashType):
+                print("Found: " + text.string)
+            else:
+                print("Trying: " + text.string)
+        except KeyboardInterrupt:
+            print("Ending...")
+            break
