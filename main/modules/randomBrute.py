f5a272ff0298b7821a8939733afbade1c1395fe2
Seth <124543219+SethG5@users.noreply.github.com>
2023-02-08 14:56:15 -0500

    Update randomBrute.py

--- main/modules/randomBrute.py
+++ main/modules/randomBrute.py
@@ -1,20 +1,23 @@
 import random
-import AntHashLib
+from modules import AntHashLib
+
 def Brute(hash, hashType, Fixed, len):
-    cracked == False
-    if Fixed == True:
-        strLen = len
-    elif Fixed == False:
-        strLen = random.Randint(1, len)
-    for i in range(0, strLen):
-        text = random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-    text = AntHashLib.String(text)
-    while cracked == False:
-        try:
-            if hash == text.hash(hashType):
-                print("Found: " + text.string)
-            else:
-                print("Trying: " + text.string)
-        except KeyboardInterrupt:
-            print("Ending...")
-            break
+  cracked = False
+  if Fixed == True:
+      strLen = len
+  while cracked == False:
+    try:
+      text = ''
+      if Fixed == False:
+        strLen = random.randint(1, len)
+      for i in range(0, strLen):
+        text += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()")
+      text = AntHashLib.String(text)
+      if hash == text.hash(hashType):
+        print("Found: " + text.string)
+        cracked = True
+      else:
+        print(f'Trying: {text.hash(hashType)} || {text.string}')
+    except KeyboardInterrupt:
+        print("Ending...")
+        break
